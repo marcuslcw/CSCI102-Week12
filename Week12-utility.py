e2b7c0cd79dd7f5fa5437904263b90752fc50b9e
Marcus Loi <cheewahloi@mymail.mines.edu>
2019-11-16 21:10:00 -0700

    UpdateString function

--- Week12-utility.py
+++ Week12-utility.py
@@ -8,3 +8,13 @@ def LoadFile(file):
             line = line.strip()
             file_list.append(line)
     return file_list
+
+def UpdateString(string_1, string_2, index_int):
+    i = 0
+    new_string = ''
+    for i in range(len(string_1)):
+        if i == index_int:
+            new_string += string_2
+        else:
+            new_string += string_1[i]    
+    return new_string
