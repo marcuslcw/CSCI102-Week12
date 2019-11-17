7377cc5bc819b43822845cacd37915ba6eb74b70
Marcus Loi <cheewahloi@mymail.mines.edu>
2019-11-16 20:51:18 -0700

    LoadFile function

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,10 @@
 def PrintOutput(string_input):
     print('OUTPUT %s' % string_input)
+
+def LoadFile(file):
+    file_list = []
+    with open(file, 'r') as o_file:
+        for line in o_file:
+            line = line.strip()
+            file_list.append(line)
+    return file_list
